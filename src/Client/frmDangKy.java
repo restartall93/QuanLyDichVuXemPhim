cd527676de1f139e5f8cd41fced1c0a1994baf88
Nguyen Quang Ha <nguyenquangha295@gmail.com>
2023-01-31 09:25:21 +0700

    update

--- src/Client/frmDangKy.java
+++ src/Client/frmDangKy.java
@@ -213,10 +213,8 @@ public class frmDangKy extends javax.swing.JFrame {
         //</editor-fold>
 
         /* Create and display the form */
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                new frmDangKy().setVisible(true);
-            }
+        java.awt.EventQueue.invokeLater(() -> {
+            new frmDangKy().setVisible(true);
         });
     }
 
